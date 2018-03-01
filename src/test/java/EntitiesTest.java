3a5c406e9e25916563a7802030af7a267ad0273c
Daniel Hogan <danhogan@carleton.cmail.ca>
2018-03-01 16:33:45 -0500

    I basic functionality to the user, perk and subscription objects and created a few tests for them.

---
+++ src/test/java/EntitiesTest.java
@@ -0,0 +1,74 @@
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.Test;
+/**
+ * Test the User, Subscription and perk classes.
+ *
+ */
+
+public class EntitiesTest {
+    private User user;
+    private Perk perk;
+    private Subscription sub;
+
+    private String email = "Test@Test.com";
+    private String pass = "pass";
+    private String namePerk = "half off test";
+    private String description = "This is a test";
+    private String nameSub = "testVisa";
+    private String fee = "once a month";
+
+    public void init()
+    {
+        user = new User(email,pass);
+        perk = new Perk(namePerk, description);
+        sub = new Subscription(nameSub,fee);
+    }
+
+    @Test
+    public void testEntities()
+    {
+        init();
+
+        sub.addPerk(perk);
+        user.addSubscription(sub);
+
+        assertThat(user.getSubscriptions().get(0).equals(sub));
+        assertThat(user.getSubscriptions().get(0).getPerks().get(0).equals(perk));
+
+        sub.removePerk(perk);
+        user.removeSubscription(sub);
+
+        assertThat(user.getSubscriptions().size() == 0);
+        assertThat(user.getSubscriptions().size() == 0);
+    }
+
+    @Test
+    public void testUser()
+    {
+        init();
+
+        assertThat(user.getEmail().equals(email));
+        assertThat(user.getPassword().equals(pass));
+    }
+
+    @Test
+    public void testPerk()
+    {
+        init();
+
+        assertThat(perk.getName().equals(namePerk));
+        assertThat(perk.getDescription().equals(description));
+        assertThat(perk.getExpiryDate() == null);
+    }
+
+    @Test
+    public void testSubscription()
+    {
+        init();
+
+        assertThat(sub.getName().equals(nameSub));
+        assertThat(sub.getFee().equals(fee));
+
+        assertThat(new Subscription(nameSub).getFee().equals("FREE"));
+    }
+}
