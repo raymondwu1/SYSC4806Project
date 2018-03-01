52e6bc75634c142359ed539d744c5bb71d1bb94c
Daniel Hogan <danhogan@carleton.cmail.ca>
2018-03-01 09:48:24 -0500

    Added dependencies and what not

---
+++ src/main/java/PerkRepository.java
@@ -0,0 +1,10 @@
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+@RepositoryRestResource(collectionResourceRel = "Perk", path = "Perk")
+public interface PerkRepository extends PagingAndSortingRepository<Perk, Long> {
+}
---
+++ src/main/java/UserRepository.java
@@ -0,0 +1,9 @@
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+@RepositoryRestResource(collectionResourceRel = "User", path = "User")
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+}
