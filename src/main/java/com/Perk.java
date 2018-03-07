41e281ba4de405c7a35af5627ff42e92d4a6160e
raymondwu <raymondwu@cmail.carleton.ca>
2018-03-07 13:34:00 -0500

    Update perks

--- src/main/java/com/Perk.java
+++ src/main/java/com/Perk.java
@@ -15,16 +15,16 @@ public class Perk {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private String name;
-    private String Description;
+    private String description;
     @ManyToOne
     private Subscription subscription;
     private java.util.Date expiryDate;
 
     /* Constructors */
-    public Perk(String name, String Description, java.util.Date expiryDate)
+    public Perk(String name, String description, java.util.Date expiryDate)
     {
         this.name = name;
-        this.Description = Description;
+        this.description = description;
         this.expiryDate = expiryDate;
     }
 
@@ -57,11 +57,11 @@ public class Perk {
     }
 
     public String getDescription() {
-        return Description;
+        return description;
     }
 
     public void setDescription(String description) {
-        Description = description;
+        description = description;
     }
 
     public Date getExpiryDate() {
--- src/main/java/com/PerkController.java
+++ src/main/java/com/PerkController.java
@@ -11,32 +11,4 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class PerkController {
 
     private PerkService perkService;
-
-    @RequestMapping(value = {"/welcome"}, method = RequestMethod.GET)
-    public String welcome(Model model) {
-        if (!model.containsAttribute("perkForm")) {
-            model.addAttribute("perkForm", new Perk());
-        }
-        return "welcome";
-    }
-
-    @RequestMapping(value = {"/welcome"}, method = RequestMethod.POST, params="method=AddPerk")
-    public String addPerk(@ModelAttribute("perkForm") Perk perkForm, BindingResult bindingResult, Model model) {
-
-        if (!perkService.existsByName(perkForm.getName())){
-            perkService.save(perkForm);
-        }
-
-        return "welcome";
-    }
-
-    @RequestMapping(value = {"/welcome"}, method = RequestMethod.POST, params="method=RemovePerk")
-    public String removePerk(@ModelAttribute("perkForm") Perk perkForm, BindingResult bindingResult, Model model) {
-
-        if (perkService.existsByName(perkForm.getName())){
-            perkService.delete(perkForm);
-        }
-
-        return "welcome";
-    }
 }
